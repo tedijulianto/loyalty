7446a9c5cf0d705240f4fb71d885b22018ec7380
tedijulianto <tedijulianto92@gmail.com>
2022-07-08 17:50:53 +0700

    Add circle loader on page profile

--- script.js
+++ script.js
@@ -61,3 +61,34 @@ $(document).ready(function () {
     },
   });
 });
+
+// let progressBar = document.querySelector(".circular-progress");
+// let valueContainer = document.querySelector(".value-container");
+
+// let progressValue = 0;
+// let progressEndValue = 35;
+// let speed = 10;
+
+// let progress = setInterval(() => {
+//   progressValue++;
+//   valueContainer.textContent = `${progressValue}M to Diamond`;
+//   progressBar.style.background = `conic-gradient(
+//       #FFBF00 ${progressValue * 3.6}deg,
+//       #FF7A00 ${progressValue * 3.6}deg
+//   )`;
+//   if (progressValue == progressEndValue) {
+//     clearInterval(progress);
+//   }
+// }, speed);
+
+let number = document.getElementById("number");
+let counter = 0;
+let counterEnd = 50;
+
+let setProgress = setInterval(() => {
+  counter++;
+  number.textContent = `${counter}M to Diamond`;
+  if (counter == counterEnd) {
+    clearInterval(setProgress);
+  }
+}, 38);
